cbdf19202b9e41c7a7e9509abf061e66b82f091d
Kiy Hong Tang <kiyhong@gmail.com>
2014-04-07 14:40:36 +0100

    added Devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 RedditRails::Application.routes.draw do
+  resources :pages
+  root :to =>"pages#index"
+
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
