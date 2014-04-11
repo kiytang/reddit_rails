fea6432a9d8885ee08228856d70dfa413f35e0c1
Kiy Hong Tang <kiyhong@gmail.com>
2014-04-11 18:26:48 +0100

    Added voting functionality

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140411093943) do
+ActiveRecord::Schema.define(version: 20140411111910) do
 
   create_table "comments", force: true do |t|
     t.integer  "user_id"
@@ -48,5 +48,13 @@ ActiveRecord::Schema.define(version: 20140411093943) do
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+
+  create_table "votes", force: true do |t|
+    t.integer  "user_id"
+    t.integer  "link_id"
+    t.boolean  "up"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
 end
