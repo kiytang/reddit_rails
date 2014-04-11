55c54319c7f7d74b73753f6724f308d0a95a6a8f
Kiy Hong Tang <kiyhong@gmail.com>
2014-04-11 11:51:40 +0100

    Completed comments MVCr

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,8 +2,14 @@ class CommentsController < ApplicationController
   before_filter :authenticate_user!
 
   def create
-   @comment = current_user.comments.create(params[:comment])
+   @comment = current_user.comments.create(post_params)
    redirect_to :back
+  end
+
+  private
+  
+  def post_params
+    params.require(:comment).permit(:user_id, :link_id, :message)
   end
 
 end
