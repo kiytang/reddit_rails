54511824f22655b6cce6d643491b1c36980a7aca
Kiy Hong Tang <kiyhong@gmail.com>
2014-04-12 10:31:33 +0100

    Added down vote functionality

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -2,14 +2,20 @@ class VotesController < ApplicationController
   before_filter :authenticate_user!
 
   def create
-   @vote = current_user.votes.create(post_params)
-   redirect_to :back
+    @vote = Vote.where(:link_id => params[:vote][:link_id], :user_id => current_user.id).first
+    if @vote
+      @vote.up = params[:vote][:up]
+      @vote.save
+    else
+      @vote = current_user.votes.create(post_params)
+    end
+    redirect_to :back
   end
 
   private
   
   def post_params
-    params.require(:vote).permit(:user_id, :link_id, :up)
+    params.require(:vote).permit(:user_id, :link_id, :up  )
   end
 
 end
