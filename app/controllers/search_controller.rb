a81afbb3fd330781d89820cda1432e7e56de3e31
Kiy Hong Tang <kiyhong@gmail.com>
2014-04-15 11:02:13 +0100

    Added limiation to the number of links using will paginate

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,5 +1,5 @@
 class SearchController < ApplicationController
   def index
-
+    @links = Link.search(:title => params[:q].page(params[:page].per_page(20)
   end
 end
--- app/views/search/index.html.rb
+++ app/views/search/index.html.rb
@@ -1,19 +1,26 @@
+<p><%= will_paginate @links %></p>
+
+Here are all the links that matched "<%= params[:q] %>"
 <ul>
   <% @links.each do |link| %>
     <li>
-      <p><%= link_to link.title, link.url %></p>
-      <p><%= link_to "coments", link %></p>
+      Votes: <%= link.votes.where(:up => true).count - link.votes.where(:up => false).count %>
+
       <% if current_user && current_user.votes.where(:link_id => link.id, :up => true).present? %>
+      *
+      <% else %>
+        <%= link_to "+", votes_path(:vote => {:link_id => link.id, :up => true}), :method => :post %>
+      <% end %>
+
+
+      <% if current_user && current_user.votes.where(:link_id => link.id, :up => false).present? %>
         *
       <% else %>
-       <%= link_to "+", votes_path(:vote => {:link_id => link.id, :up => true}), :method => :post %>
-       <%= link_to "-", votes_path(:vote => {:link_id => link.id, :up => false}), :method => :post %>
-       <p>Votes: <%= link.votes.where(:up => true).count - link.votes.where(:up => false).count %></p>
+          <%= link_to "-", votes_path(:vote => {:link_id => link.id, :up => false}), :method => :post %>
       <% end %>
+
+      <%= link_to link.title, link.url %>
+      <%= link_to "comments", link %>
     </li>
   <% end %>
 </ul>
-
-<%= link_to "Add a link", new_link_path %>
-
-<p><%= will_paginate @links %></p>
