9f457ea0cf563983aa7f42f5feb737d48dc30688
Kiy Hong Tang <kiyhong@gmail.com>
2014-04-15 16:48:44 +0100

    added integration test

--- test/integration/link_create_test.rb
+++ test/integration/link_create_test.rb
@@ -2,12 +2,32 @@ require 'test_helper'
 
 class LinkCreateTest < ActionDispatch::IntegrationTest
 
-test "the world is round" do
-  assert true
-end
+  test "logged in user submits valid links"do
+    user = User.create(:email => "#{Time.now.to_f}@example.com", :password => "password")
+    visit new_user_session_path
+    assert_equal '/users/sign_in', current_path
+    
+    fill_in 'user_email', :with => user.email
+    fill_in 'user_password', :with => 'password'
+    
+    click_button 'Sign in'
+    refute_equal new_user_session_path, current_path
+    
+    save_and_open_page
 
-test "logged in user submits valid links"do
-  user = User.create(:email => "#{Time.now.to_f}@example.com", :password => "password")
+    visit new_link_path
+    title = "Random Title: #{Time.now.to_f}"
+    fill_in 'link_title', "with => title"
+    fil_in 'link_url', :with => 'http://schneems.com'
+    click_button 'Create Link'
 
-end
+    link = Link.last
+    assert_equal link_path(link), current_path
+
+    assert has_content?(link.title)
+    assert has_content?(link.url)
+  end
+
+
+
 end
